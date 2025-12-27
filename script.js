31883f2bab618ee09427ed05275e3f28cc127120
pedrojesusdev <pa04052007@gmail.com>
2025-12-26 22:33:30 -0300

    Adding smoth scroll in page

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.nav-links a[href^="#"], a[href^="#projects"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
